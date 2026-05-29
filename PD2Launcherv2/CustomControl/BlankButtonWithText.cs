c059090f958ad54706e89e6aeb2a921aaef6e166
Slacker <288981238+Slacker86@users.noreply.github.com>
2026-05-29 17:40:23 +0200

    PD2Launcherv2: CustomControl: Introduce BlankButtonWithText
    ...and its set of images

---
+++ PD2Launcherv2/CustomControl/BlankButtonWithText.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PD2Launcherv2.CustomControl
+{
+    public class BlankButtonWithText : Button
+    {
+        static BlankButtonWithText()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BlankButtonWithText), new FrameworkPropertyMetadata(typeof(BlankButtonWithText)));
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(BlankButtonWithText),
+            new PropertyMetadata("..."));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+    }
+}
