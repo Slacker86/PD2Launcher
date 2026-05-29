f59f9dfbe007dceca1d346e1182b164ad598551e
Slacker <288981238+Slacker86@users.noreply.github.com>
2026-05-29 17:40:22 +0200

    PD2Launcherv2: CustomImageButton: Add DisabledImageSource
    ...to be displayed when disabling a button.
    
    Also, add a static MissingImage and use it for DisabledImageSource by default.

--- PD2Launcherv2/CustomControl/CustomImageButton.cs
+++ PD2Launcherv2/CustomControl/CustomImageButton.cs
@@ -1,11 +1,36 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace PD2Launcherv2.CustomControl
 {
     public class CustomImageButton : Button
     {
+        private static RenderTargetBitmap _missingImage = null!;
+        private static BitmapSource MissingImage
+        {
+            get
+            {
+                if (_missingImage == null)
+                {
+                    Rect imageSize = new(new Size(100, 100));
+
+                    _missingImage = new RenderTargetBitmap((int)imageSize.Width, (int)imageSize.Height, 96, 96, PixelFormats.Pbgra32);
+
+                    DrawingVisual visual = new();
+                    using (DrawingContext context = visual.RenderOpen())
+                    {
+                        context.DrawRectangle(Brushes.Magenta, pen: null, imageSize);
+                    }
+
+                    _missingImage.Render(visual);
+                }
+
+                return _missingImage;
+            }
+        }
+
         static CustomImageButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomImageButton), new FrameworkPropertyMetadata(typeof(CustomImageButton)));
@@ -23,6 +48,12 @@ namespace PD2Launcherv2.CustomControl
             typeof(CustomImageButton),
             new PropertyMetadata(default(ImageSource)));
 
+        public static readonly DependencyProperty DisabledImageSourceProperty = DependencyProperty.Register(
+            "DisabledImageSource",
+            typeof(ImageSource),
+            typeof(CustomImageButton),
+            new PropertyMetadata(MissingImage));
+
         public ImageSource NormalImageSource
         {
             get => (ImageSource)GetValue(NormalImageSourceProperty);
@@ -33,6 +64,12 @@ namespace PD2Launcherv2.CustomControl
         {
             get => (ImageSource)GetValue(PressedImageSourceProperty);
             set => SetValue(PressedImageSourceProperty, value);
+        }
+
+        public ImageSource DisabledImageSource
+        {
+            get => (ImageSource)GetValue(DisabledImageSourceProperty);
+            set => SetValue(DisabledImageSourceProperty, value);
         }
     }
 }
