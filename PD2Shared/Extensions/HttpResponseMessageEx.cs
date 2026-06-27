160a5b95107294bd73cce9a723e09432a1624df5
Slacker <288981238+Slacker86@users.noreply.github.com>
2026-06-27 19:26:45 +0200

    PD2Shared: Introduce Extensions.HttpResponseMessageEx

---
+++ PD2Shared/Extensions/HttpResponseMessageEx.cs
@@ -0,0 +1,17 @@
+﻿namespace PD2Shared.Extensions
+{
+    public static class HttpResponseMessageEx
+    {
+        // An alternative to EnsureSuccessStatusCode() since it only produces HttpRequestExceptions with (questionably useful) messages such as:
+        // 'Response status code does not indicate success: 404 (Not Found).'
+        public static void ThrowIfUnsuccessful(this HttpResponseMessage httpResponseMessage)
+        {
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                var reqMsg = httpResponseMessage.RequestMessage!;
+
+                throw new HttpRequestException($"{reqMsg.Method} '{reqMsg.RequestUri}' failed with: {(int)httpResponseMessage.StatusCode} ({httpResponseMessage.StatusCode})", inner: null, httpResponseMessage.StatusCode);
+            }
+        }
+    }
+}
